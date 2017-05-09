b96d3d9cdff1c846fa93924d933828c868f92460
Mohammad Mohsen Ghannam <mohammad.mohsen@hotmail.com>
2017-05-09 08:05:31 +0200

    changed structure of grammar element productions

--- first/first_of.py
+++ first/first_of.py
@@ -1,50 +1,48 @@
 from grammar_elements import *
 from grammar_utils.get_nonterminal_children import get_non_terminal_children
 from generic_helpers.decorators import memoize
+from grammar_reader import reader
 
 # Don't cycle
 __first_of_visited = []
 
 
-def has_first_epsilon(el: NonTerminal):
+def has_first_epsilon(el: GrammarElement):
     """
     Epsilon is added to first, iff the non terminal has and epsilon or all its children have an epsilon 
     """
-    return el.has_epsilon() or all([child.has_epsilon() for child in get_non_terminal_children(el)])
+    if el.type == ElementType.Terminal or el.type == ElementType.Epsilon:
+        return False
+    else:  # in case of a non terminal
+        return el.has_epsilon() or all([child.has_epsilon() for child in get_non_terminal_children(el)])
 
 
 @memoize
-def __first_of(parent: NonTerminal):
+def __first_of(parent: GrammarElement):
     """
     Returns the first of this non terminal, without considering the epsilons
     """
     global __first_of_visited
     result = []
-    if len(parent.rule) == 0:
-        return []  # Non terminal has no children
+    if parent.type == ElementType.Terminal or parent.type == ElementType.Epsilon:  # case it is a terminal
+        return [parent]
 
-    for (i, current_element) in enumerate(parent.rule):
+    for production in parent.productions:
+        for (i, current_element) in enumerate(production):
 
-        if i == 0 and current_element.type == ElementType.Terminal:
-            result.append(current_element)
+            if i == 0 and current_element.type == ElementType.Terminal:
+                result.append(current_element)
 
-        previous_element = parent.rule[i - 1]
+            if current_element in __first_of_visited:
+                continue
 
-        if current_element in __first_of_visited or current_element.type == ElementType.OrOperation:
-            continue
-
-        elif previous_element.type == ElementType.OrOperation \
-                and current_element.type == ElementType.Terminal:
-            result.append(current_element)
-
-        # Met an OR operation, recursively check if `el` is in the `first` of the following
-        elif (previous_element.type == ElementType.OrOperation or i == 0) \
-                and current_element.type == ElementType.NonTerminal:
-            # print("  ", current_element.name, "  ", current_element.type)
-            # Append before diving, to avoid cycles
-            __first_of_visited.append(current_element)
-            for e in __first_of(current_element):
-                result.append(e)
+            # Met a NonTerminal object in the first of a production,
+            #  recursively check if `el` is in the `first` of the following
+            elif (i == 0) and current_element.type == ElementType.NonTerminal:
+                # Append before diving, to avoid cycles
+                __first_of_visited.append(current_element)
+                for e in __first_of(current_element):
+                    result.append(e)
 
     return result
 
@@ -61,3 +59,22 @@ def get_first(el: NonTerminal):
         result.append(Terminal.create_epsilon())
 
     return result
+
+
+def has_epsilon_in_list(list):
+    return all([el.type == ElementType.Epsilon for el in list])
+
+
+def get_first_of_list(list_of_non_terminals):
+    if len(list_of_non_terminals) == 0: return []
+    first = get_first(list_of_non_terminals[0])
+    if has_epsilon_in_list(first):
+        return first + get_first_of_list(list_of_non_terminals[1:])
+    else:
+        return first
+
+result = reader.from_string("""E ::= T E'
+E' ::= + T E' | \L
+T ::= F T'
+T' ::= * F T' | \L
+F ::= ( E ) | id""")
